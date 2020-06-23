19389530e8a589917f9263a3ec3ea9b52617708d
Jérémie <jeremie.bruhwiler@epitech.eu>
2020-06-23 10:16:28 +0200

    First push

---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-layout justify-center>
+      <v-card
+        class="d-flex justify-center mb-6"
+        flat
+        tile
+      >
+        <h1>(404) Page Not Found</h1>
+      </v-card>
+    </v-layout>
+    <v-layout justify-center>
+      <v-card-actions>
+        <v-btn primary>
+          <router-link to='/'><span>Go Back</span></router-link>
+        </v-btn>
+
+      </v-card-actions>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
